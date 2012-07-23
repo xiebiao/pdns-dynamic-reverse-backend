08dd8e3ea1cf0a5565b8c852058762ee04ba372c
xiaog <joyrap@gmail.com>
2012-07-23 12:45:42 +0800

    add tools

---
+++ tools/insert_dns_record.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python 
+# --*-- coding: utf-8 --*--
+
+#import _mysql
+import MySQLdb
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Use "-i" for insert data,"-t" for truncate data.')
+parser.add_argument("-t",action="store_true",help="truncate data")
+parser.add_argument("-i",action="store_true",help="insert data")
+_insert = parser.parse_args().i
+_truncate = parser.parse_args().t
+if _insert and _truncate:
+	parser.print_help()
+	import sys
+	sys.exit(0)
+
+_MYSQL_HOST="10.28.168.163"
+_MYSQL_USER="pdns"
+_MYSQL_PASSWORD="pdns"
+_MYSQL_DB="pdns"
+_TTL="68400"
+_A="10.28.164.231"
+
+dns_file = open("test",'r')
+
+def insert():
+    lines = dns_file.readlines()
+    con = get_connection()
+    cursor = con.cursor()
+    for line in lines:
+        linea = line.split(" ")
+        domain = linea[0]
+        param = (domain,"MASTER")
+        sql = "insert into domains (name,type) values(%s,%s)"
+        cursor.execute(sql,param)
+        sql_id = "select id from domains order by id desc limit 1 "
+        cursor.execute(sql_id)
+        rs = cursor.fetchone()
+        domain_id=rs[0]
+        sql_record="""insert into records
+        (domain_id,name,type,content,ttl,prio,change_date) values
+        (%s,%s,%s,%s,%s,%s,%s)"""
+        param = (domain_id,
+                domain,
+                "SOA",
+                " 2012071800 28800 7200 604800 86400",
+                _TTL,
+                "0",
+                time.time())
+        param_a = (domain_id,
+                domain,
+                "A",
+                _A,
+                _TTL,
+                "0",
+                time.time())
+        print param
+        print param_a
+        cursor.execute(sql_record,param)
+        cursor.execute(sql_record,param_a)
+        con.commit()
+    close(con)
+
+def truncate():
+    sql1 = "truncate table domains"
+    sql2 = "truncate table records"
+    con1 = get_connection()
+    con1.query(sql1)
+    con2 = get_connection()
+    con2.query(sql2)
+    print "%s\n%s" % (sql1,sql2)
+    close(con1)
+    close(con2)
+
+def close(connection):
+    connection.close()
+
+def get_connection():
+    connection =MySQLdb.connect(
+        _MYSQL_HOST,
+        _MYSQL_USER,
+        _MYSQL_PASSWORD,
+        _MYSQL_DB)
+    return connection
+
+if __name__   == "__main__":
+    if _insert == True :
+        insert()
+    if _truncate == True:
+        truncate()
---
+++ tools/queryperf_test_record.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python 
+# --*-- coding: utf-8 --*--
+"""
+    生成queryperf压力测试文件
+"""
+import os
+import _mysql
+import random
+
+_DNS_TEST_FILE="test"
+_DNS_RECORDS=100000
+_DOMAIN_LENGTH=10
+
+def get_random_string(n):
+
+    st=''
+    while len(st)<n:
+        temp= chr(97+random.randint(0,25))
+        if st.find(temp)==-1:
+            st = st.join(['',temp])
+    return st
+
+def create_test_file():
+    
+    test_file = open(_DNS_TEST_FILE,'w+') 
+    dns_records = _DNS_RECORDS
+    while dns_records > 0:
+        dns_records = dns_records - 1 
+        n = random.randint(1,_DOMAIN_LENGTH)
+        domain="www."+ get_random_string(n)+ str(dns_records) +".com A\n" 
+        #print domain
+        test_file.write(domain)
+
+    test_file.close()
+
+create_test_file()
+
